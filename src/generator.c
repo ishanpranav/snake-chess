1b4cf6e652b7f04c06c5c5cedaa2cbc98ec6d3bb
ishanpranav <ishan.pranav@outlook.com>
2024-05-16 14:14:31 -0700

    P5

--- src/generator.c
+++ src/generator.c
@@ -9,6 +9,7 @@
 
 uint64_t pawnAttacks[COLORS][SQUARES];
 uint64_t knightAttacks[SQUARES];
+uint64_t kingAttacks[SQUARES];
 
 static uint64_t generate_white_pawn_attacks(Square square)
 {
@@ -115,14 +116,71 @@ static uint64_t generate_knight_attacks(Square square)
     return result;
 }
 
+static uint64_t generate_king_attacks(Square square)
+{
+    uint64_t value = 0;
+    uint64_t result = 0;
+
+    bitboard_set(value, square);
+
+    uint64_t a = value >> 1;
+
+    if (a & BITBOARD_NOT_FILE_H)
+    {
+        result |= a;
+    }
+
+    uint64_t b = value >> 7;
+
+    if (b & BITBOARD_NOT_FILE_A)
+    {
+        result |= b;
+    }
+
+    result |= (value >> 8);
+
+    uint64_t d = value >> 9;
+
+    if (d & BITBOARD_NOT_FILE_H)
+    {
+        result |= d;
+    }
+
+    uint64_t e = value << 1;
+
+    if (e & BITBOARD_NOT_FILE_A)
+    {
+        result |= e;
+    }
+
+    uint64_t f = value << 7;
+
+    if (f & BITBOARD_NOT_FILE_H)
+    {
+        result |= f;
+    }
+
+    result |= (value << 8);
+
+    uint64_t h = value << 9;
+
+    if (h & BITBOARD_NOT_FILE_A)
+    {
+        result |= h;
+    }
+
+    return result;
+}
+
 static void leaper_attack_table()
 {
     for (Square square = 0; square < SQUARES; square++)
     {
         pawnAttacks[COLOR_WHITE][square] = generate_white_pawn_attacks(square);
         pawnAttacks[COLOR_BLACK][square] = generate_black_pawn_attacks(square);
-        knightAttacks[square] = generate_knight_attacks(square);
-        bitboard_write_string(stdout, knightAttacks[square]);
+        kingAttacks[square] = generate_king_attacks(square);
+
+        bitboard_write_string(stdout, kingAttacks[square]);
     }
 }
 
