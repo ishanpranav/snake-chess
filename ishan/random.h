d2940b7cdb63a4a839349d7301b3ce033f1814fc
ishanpranav <ishan.pranav@outlook.com>
2024-05-16 18:31:59 -0700

    Add xorshift32_random64

--- ishan/random.h
+++ ishan/random.h
@@ -31,3 +31,13 @@ long long xorshift32_random(Object instance, long long max);
  * @return A uniformly distributed random 32-bit integer in the interval.
 */
 uint32_t xorshift32_random32(Object instance);
+
+/**
+ * Generates a random 64-bit integer using multiple calls to Marsaglia's
+ * xorshift32 (exclusive or, 32-bit) algorithm, and returns a value aligned to
+ * exactly 64 bits.
+ *
+ * @param instance the instance
+ * @return A uniformly distributed random 64-bit integer in the interval.
+*/
+uint64_t xorshift32_random64(Object instance);
