1ca4d54ce04755f5096dc0e656bf833a5f059de1
Ishan Pranav <ishan.pranav@outlook.com>
2024-05-22 10:33:32 -0700

    Make castle move

--- src/generator.c
+++ src/generator.c
@@ -8,24 +8,34 @@
 #include "../lib/move.h"
 #include "../lib/spawner.h"
 
+static void relocate(Board board, Piece piece, uint64_t source, uint64_t target)
+{
+    board->pieces[piece] &= ~source;
+    board->pieces[piece] |= target;
+}
+
 static void make_move(Board board, Move move)
 {
-    board->pieces[move->piece] &= ~bitboard(move->source);
-    board->pieces[move->piece] |= bitboard(move->target);
+    uint64_t source = bitboard(move->source);
+    uint64_t target = bitboard(move->target);
+
+    relocate(board, move->piece, source, target);
 
     int direction;
     Piece enemyBegin;
-    uint64_t target = bitboard(move->target);
+    Piece friendBegin;
 
     if (board->color)
     {
         direction = -1;
         enemyBegin = PIECE_WHITE_PAWN;
+        friendBegin = PIECE_BLACK_PAWN;
     }
     else
     {
         direction = 1;
         enemyBegin = PIECE_BLACK_PAWN;
+        friendBegin = PIECE_WHITE_PAWN;
     }
 
     if (move->type & MOVE_TYPES_CAPTURE)
@@ -45,30 +55,23 @@ static void make_move(Board board, Move move)
 
     if (move->type & MOVE_TYPES_PROMOTION)
     {
-        Piece friendOffset = 0;
-
-        if (board->color)
-        {
-            friendOffset += PIECE_BLACK_PAWN;
-        }
-
-        board->pieces[friendOffset + PIECE_PAWN] &= ~target;
+        board->pieces[friendBegin + PIECE_PAWN] &= ~target;
 
         if (move->type & MOVE_TYPES_KNIGHT)
         {
-            board->pieces[friendOffset + PIECE_KNIGHT] |= target;
+            board->pieces[friendBegin + PIECE_KNIGHT] |= target;
         }
         else if (move->type & MOVE_TYPES_BISHOP)
         {
-            board->pieces[friendOffset + PIECE_BISHOP] |= target;
+            board->pieces[friendBegin + PIECE_BISHOP] |= target;
         }
         else if (move->type & MOVE_TYPES_ROOK)
         {
-            board->pieces[friendOffset + PIECE_ROOK] |= target;
+            board->pieces[friendBegin + PIECE_ROOK] |= target;
         }
         else if (move->type & MOVE_TYPES_QUEEN)
         {
-            board->pieces[friendOffset + PIECE_QUEEN] |= target;
+            board->pieces[friendBegin + PIECE_QUEEN] |= target;
         }
     }
 
@@ -84,6 +87,24 @@ static void make_move(Board board, Move move)
     if (move->type & MOVE_TYPES_DOUBLE_PUSH)
     {
         board->enPassant = move->target + direction * FILES;
+    }
+
+    if (move->type & MOVE_TYPES_CASTLE_KINGSIDE)
+    {
+        relocate(
+            board, 
+            friendBegin + PIECE_ROOK, 
+            bitboard(move->source + 3), 
+            bitboard(move->source + 1));
+    }
+
+    if (move->type & MOVE_TYPES_CASTLE_QUEENSIDE)
+    {
+        relocate(
+            board,
+            friendBegin + PIECE_ROOK,
+            bitboard(move->source - 4),
+            bitboard(move->source - 1));
     }
 }
 
