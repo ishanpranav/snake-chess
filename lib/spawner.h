4c9ac5d8025c9fd0a867fea97bf18c7ce27da326
Ishan Pranav <ishan.pranav@outlook.com>
2024-05-22 00:58:51 -0700

    Document move spawner

--- lib/spawner.h
+++ lib/spawner.h
@@ -5,4 +5,10 @@
 #include "attack_table.h"
 #include "board.h"
 
+/**
+ * 
+ * @param results
+ * @param board
+ * @param table
+*/
 void spawner_generate_moves(List results, Board board, AttackTable table);
